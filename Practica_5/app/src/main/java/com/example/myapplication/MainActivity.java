07a80a7c8b56d1afe7a829de52bc5d59794db589
Castilla33 <mcastilla3389@gmail.com>
2025-11-06 19:14:34 +0100

    Ejercicio login hecho

---
+++ Practica_5/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.myapplication;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText correo;
+    private EditText contra;
+    private Button btnContinuar;
+    private Switch switchRecordar;
+    private TextView alerta;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        correo = findViewById(R.id.campoCorreo);
+        contra = findViewById(R.id.campoContra);
+        btnContinuar = findViewById(R.id.btnContinuar);
+        switchRecordar = findViewById(R.id.switchRecordar);
+        alerta = findViewById(R.id.textoAlerta);
+
+        btnContinuar.setOnClickListener(v -> {
+
+            String stringCorreo = correo.getText().toString();
+            String stringContra = contra.getText().toString();
+
+            if (stringCorreo.equals("correo@correo.com") && stringContra.equals("123")){
+                if (switchRecordar.isChecked()){
+
+                    alerta.setText("Usuario y contraseña correctos, será recordado para el siguiente acceso");
+                    alerta.setTextColor(Color.GREEN);
+
+                } else {
+                    alerta.setText("Usuario y contraseña correctos");
+                    alerta.setTextColor(Color.GREEN);
+                }
+
+            } else {
+                alerta.setText("Usuario y contraseña incorrectos");
+                alerta.setTextColor(Color.RED);
+            }
+
+        });
+
+    }
+}
